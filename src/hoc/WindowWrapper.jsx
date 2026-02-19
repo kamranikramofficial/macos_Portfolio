5cf982c7a14b1cfb1fe3b3928a976bdc7f656c0c
kamranikramofficial <kamranikramofficial@gmail.com>
2026-02-19 12:10:45 +0500

    add some new in terminal windows

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -1,12 +1,25 @@
-import React, { useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 
 import useWindowStore from "#store/window.js";
+import { useGSAP } from "@gsap/react";
 
 const WindowWrapper = (Component, windowkey) => {
   const Wrapped = (props) => {
     const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowkey];
     const ref = useRef(null);
+
+  useGSAP(() => {
+    const el = ref.current;
+    if (!el || !isOpen) return;
+
+    el.style.display = "block";
+  }, [isOpen]);
+  useLayoutEffect(() => {
+      const el = ref.current;
+      if (!el) return;
+      el.style.display = isOpen ? "block" : "none";
+  }, [isOpen]);
 
     return (
       <section id={windowkey} ref={ref} style={{ zIndex }} className="absolute">
